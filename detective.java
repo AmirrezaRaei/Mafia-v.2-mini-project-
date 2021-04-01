15b89109b42f3db4c012a2741ae9ad3587dba593
Amirreza <amir1381raei@gmail.com>
2021-04-01 16:41:56 +0430

    add playerchoose

--- detective.java
+++ detective.java
@@ -1,5 +1,6 @@
 public class detective extends Player{
     protected boolean useAbility = false;
+    protected String playerChoose;
     public detective(String name, String role) {
         super(name, role);
         super.wakeupAtNight = true;
