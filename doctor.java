28b9345a611d1112f848231f2d1cca4f588fdd34
Amirreza <amir1381raei@gmail.com>
2021-04-01 16:41:08 +0430

    add playerchoose

--- doctor.java
+++ doctor.java
@@ -1,5 +1,6 @@
 public class doctor extends Player{
     protected boolean useAbility = false;
+    protected String playerChoose;
     public doctor(String name, String role) {
         super(name, role);
         super.wakeupAtNight = true;
